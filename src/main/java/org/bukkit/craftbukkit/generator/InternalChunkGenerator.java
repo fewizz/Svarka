233ff04083106ab64d0b14154e213031999b2991
fewizz <hazeevaidar@yandex.ru>
2016-07-07 00:53:15 +0300

    1.10.2(Atm without craftbukkit patches, but with some optimizations)

--- src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.bukkit.craftbukkit.generator;
-
-import org.bukkit.generator.ChunkGenerator;
-
-import net.minecraft.world.chunk.IChunkGenerator;
-
-// Do not implement functions to this class, add to NormalChunkGenerator
-public abstract class InternalChunkGenerator extends ChunkGenerator implements IChunkGenerator {
-}
