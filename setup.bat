1b9c06f82b0f6ec2c7021bf3971daade34b7bce7
fewizz <hazeevaidar@yandex.ru>
2016-08-04 06:46:04 +0300

    [1.10.2] FROM ZERO.

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
-gradlew setup -Dfile.encoding=utf-8
+gradlew setup -Dfile.encoding=utf-8 -Dorg.gradle.jvmargs=-Xmx3G
 STOP
---
+++ src/main/java/JustClass.java
@@ -0,0 +1,6 @@
+public class JustClass {
+	
+	public void hello() {
+		System.out.println("Hello!");
+	}
+}
